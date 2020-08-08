40238d330d15cd6f88dc44a8f418758c94d8309e
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-09 00:27:08 +0530

    Solution for Chef War - Augst 2020 Long challege

--- src/Long Challeges/August2020/Chef Wars/Program.cs
+++ src/Long Challeges/August2020/Chef Wars/Program.cs
@@ -2,11 +2,32 @@
 
 namespace Chef_Wars
 {
+    /// <summary>
+    /// Problem statement : https://www.codechef.com/AUG20B/submit/CHEFWARS
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfTestCases = Convert.ToInt32(Console.ReadLine());
+            while (numberOfTestCases-- > 0)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                int health = Convert.ToInt32(input[0]);
+                int power = Convert.ToInt32(input[1]);
+
+                while (power > 0 && health > 0)
+                {
+                    health = health - power;
+                    power = power / 2;
+                }
+
+                if (power >= health)
+                    Console.WriteLine("1");
+                else
+                    Console.WriteLine("0");
+            }
+            Console.ReadLine();
         }
     }
 }
