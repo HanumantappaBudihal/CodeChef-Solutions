df4c17dd9f6c866d7dc64f587d76265a5c96d548
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-09 00:24:10 +0530

    Initial Setup for Code chef solutions

---
+++ src/Long Challeges/August2020/Chef Wars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chef_Wars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
