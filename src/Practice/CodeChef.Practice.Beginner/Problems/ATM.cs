dd1a895f0e4e0ed71aa0de19f70358d9e4e88415
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-23 01:00:15 +0530

    Code chef -> Practice ->  Begineer : Project and ATM problem solution

---
+++ src/Practice/CodeChef.Practice.Beginner/Problems/ATM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChef.Practice.Beginner.Problems
+{
+    //Problem statement : https://www.codechef.com/problems/HS08TEST
+    //My solution :  https://www.codechef.com/viewsolution/37060099
+    static class ATM
+    {
+        public static void ATM_Main()
+        {
+            //Read input
+            string[] inputStrings = Console.ReadLine().Split(' ');
+
+            //Divide the input values
+            decimal withDrawAmount = Convert.ToDecimal(inputStrings[0]);
+            decimal totalAmount = Convert.ToDecimal(inputStrings[1]);
+
+            //Computation
+            if (totalAmount >= withDrawAmount + 0.5m && withDrawAmount % 5 == 0)
+                Console.WriteLine(totalAmount - withDrawAmount - 0.5m);
+            else
+                Console.WriteLine(totalAmount);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ src/Practice/CodeChef.Practice.Beginner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CodeChef.Practice.Beginner.Problems;
+namespace CodeChef.Practice.Beginner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Code chef : Practice -> Beginner");
+
+            //1. ATM
+            ATM.ATM_Main();
+        }
+    }
+}
